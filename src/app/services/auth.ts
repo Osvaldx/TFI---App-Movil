23d66b2ba94b58a47a693297b59de8ab8ed3280b
osvaldx <romanonahuel24@gmail.com>
2025-08-30 18:12:18 -0300

    [+] Se implemento un BehaviorSubject para poder guardar los datos del user

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class Auth {
+
   private supabase: SupabaseClient;
+  // Creamos la "fuente" para poder establecer un estado: nos va a servir para saber si esta login o no
+  private _user$ = new BehaviorSubject<User | null>(null);
+  // Exponemos una variable que solo es el Observador para poder ver el estado y sin manera de editarlo
+  user$ = this._user$.asObservable();
 
   constructor() {
     this.supabase = createClient(
       environment.SUPABASE_API_URL,
       environment.SUPABASE_API_KEY,
     );
+
+    //Vemos si existe en algun lado la sesion iniciada y la guardamos en _user$
+    this.supabase.auth.getUser()
+    .then(({ data }) => { // Desestructuramos data
+      this._user$.next(data.user ?? null); // Guardamos user en la fuente si esq existe, sino null
+    })
+
+    //Si llega a cambiar la session hay que cambiar el estado
+    this.supabase.auth.onAuthStateChange((_event, session) => {
+      this._user$.next(session?.user ?? null);
+    })
   }
 
   async signUp(email: string, password: string) {
@@ -18,10 +35,17 @@ export class Auth {
   }
 
   async signIn(email: string, password: string) {
-    return this.supabase.auth.signInWithPassword({ email, password });
+    const resp = await this.supabase.auth.signInWithPassword({ email, password });
+
+    if(resp.error) throw resp.error;
+
+    this._user$.next(resp.data.user ?? null);
+    return resp
   }
 
   async signOut() {
-    return this.supabase.auth.signOut();
+    const resp = await this.supabase.auth.signOut();
+    this._user$.next(null);
+    return resp
   }
 }
