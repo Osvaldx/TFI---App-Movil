f9d468b1f03454f9c0a43d1c587b7ab6fe62f8d0
osvaldx <romanonahuel24@gmail.com>
2025-08-30 16:25:14 -0300

    [+] Se implemento Notify el servicio para utilizar toasts y loadings

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,64 +1,54 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { IonContent, IonButton, AlertController, LoadingController } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { CustomInputComponent } from "src/app/components/custom-input/custom-input.component";
 import { Auth } from 'src/app/services/auth';
 import { Router } from '@angular/router';
 import { LogoComponent } from 'src/app/components/logo/logo.component';
+import { Notify } from 'src/app/services/notify';
+import { ToastColors } from 'src/app/enums/list-colors-toast';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, CommonModule, FormsModule, HeaderComponent, CustomInputComponent, ReactiveFormsModule, LogoComponent]
+  imports: [IonIcon, IonButton, IonContent, CommonModule, FormsModule, HeaderComponent, CustomInputComponent, ReactiveFormsModule, LogoComponent]
 })
 export class AuthPage implements OnInit {
 
   authService = inject(Auth)
   router = inject(Router)
+  notify = inject(Notify)
 
   loginForm = new FormGroup({
     email: new FormControl('',[Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required])
   })
 
-  constructor(private alertController: AlertController, private loading: LoadingController) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  async buildAlert(header: string, subHeader: string, message: string, buttons: string[]) {
-    const alert = await this.alertController.create({
-      header: header,
-      subHeader: subHeader,
-      message: message,
-      buttons: buttons,
-      backdropDismiss: false
-    })
-
-    await alert.present();
-    await alert.onDidDismiss();
-    return
-  }
-
-  onSubmit() {
+  async signIn() {
     if(this.loginForm.controls.email.value === "" || this.loginForm.controls.password.value === "") {
-      this.buildAlert("[!] Intento Invalido", "Complete todos los campos", "Necesita completar todos los campos para poder ingresar", ["Entendido","No entendi"]);
+      await this.notify.buildToast(ToastColors.red, "Complete todos los campos", 2000, "close-circle-outline")
       return
     }
-
+    
     this.authService.signIn(this.loginForm.controls.email.value!, this.loginForm.controls.password.value!)
-    .then((resp) => {
+    .then(async (resp) => {
       console.log(resp);
       if(resp.data.user?.role === "authenticated") {
+        await this.notify.buildToast(ToastColors.green, "Inicio de sesion exitoso", 1000, "checkmark-circle-outline")
         this.router.navigateByUrl("home");
       }
-
+      
       if(resp.error) {
-        this.buildAlert("[!] Credeciales invalidas", "Usuario no existente o datos incorrectos", "Porfavor, intente nuevamente para poder iniciar", ["Entendido","No entendi"])
+        await this.notify.buildToast(ToastColors.red, "Credenciales invalidas", 20000, "close-circle-outline")
       }
     })
     .catch((err) => {
@@ -66,19 +56,8 @@ export class AuthPage implements OnInit {
     })
   }
 
-  async showLoading(msj: string, duration: number) {
-    const load = await this.loading.create({
-      message: msj,
-      duration: duration,
-    })
-
-    await load.present();
-    await load.onDidDismiss();
-    return
-  }
-
   async sendPageSignUp() {
-    await this.showLoading("Enviando al registro...", 500);
+    await this.notify.buildLoading("Enviando al registro...", 500);
     await this.router.navigateByUrl("sign-up");
   }
 }
