9ef677f4a06ff35883fb1fa3b65f62d80c836f82
osvaldx <romanonahuel24@gmail.com>
2025-08-30 18:13:18 -0300

    [+] Se inicio la suscripcion al Auth

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,21 +1,29 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonButton } from '@ionic/angular/standalone';
 import { Auth } from 'src/app/services/auth';
 import { Router } from '@angular/router';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { distinctUntilChanged, map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, CommonModule, FormsModule, HeaderComponent]
 })
 export class HomePage implements OnInit {
 
   authService = inject(Auth)
   router = inject(Router)
+
+  email$ = this.authService.user$.pipe(
+    map(
+      (usr) => usr?.email ?? "")
+      ,distinctUntilChanged() // Para que si no cambio el mail no haga nada
+    );
 
   constructor() { }
 
@@ -25,7 +33,6 @@ export class HomePage implements OnInit {
   signOut() {
     this.authService.signOut()
     .then((resp) => {
-      console.log(resp);
       if(resp.error === null) {
         this.router.navigateByUrl("/", { replaceUrl: true });
       }
