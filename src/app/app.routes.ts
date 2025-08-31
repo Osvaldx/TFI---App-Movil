866180f2cac76e96b391130a03f6beb8e0542510
osvaldx <romanonahuel24@gmail.com>
2025-08-31 16:46:20 -0300

    [+] se le agrego el guard a la ruta home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'auth',
+    redirectTo: 'splash',
     pathMatch: 'full',
   },
   {
@@ -12,10 +13,17 @@ export const routes: Routes = [
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage),
+    canActivate: [authGuard]
   },
   {
     path: 'sign-up',
     loadComponent: () => import('./pages/auth/sign-up/sign-up.page').then( m => m.SignUpPage)
   },
+  {
+    path: 'splash',
+    loadComponent: () => import('./pages/splash/splash.page').then( m => m.SplashPage)
+  },
+
+  { path: "**", redirectTo: "splash", pathMatch: "full"},
 ];
