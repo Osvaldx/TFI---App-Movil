4847c1fcfd786c16fbb56e73775da56fcc563e06
osvaldx <romanonahuel24@gmail.com>
2025-08-30 22:58:19 -0300

    [+] configuracion para los splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,30 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nahuel.login',
-  appName: 'PruebaApp',
+  appId: 'com.osvaldx.app',
+  appName: 'OsvaldApp',
   webDir: 'www',
   plugins: {
     StatusBar: {
       overlaysWebView: false,
       style: 'LIGHT',
       backgroundColor: '#ffffff'
+    },
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#23bb44",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: false
     }
   }
 };
